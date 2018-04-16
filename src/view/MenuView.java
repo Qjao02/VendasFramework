d32455b61a7156a70f1c02d8ed39564d04637f0b
Daniel-PC\Daniel <danieldomingueti@gmail.com>
2018-04-15 21:04:19 -0300

    Adicionado Menu Venda

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -3,9 +3,14 @@ package view;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import FrameworkMIF.*;
+import FrameworkMIF.Action;
+import FrameworkMIF.Menu;
 import controller.ClienteController;
+import controller.ProdutoController;
+import controller.VendaController;
 import model.Cliente;
+import model.Produto;
+import model.Venda;
 
 
 
@@ -13,6 +18,11 @@ public class MenuView {
 	
 	public static ArrayList<Cliente> clientes;
 	public static ClienteController clienteController;
+	public static ArrayList<Produto> produtos;
+	public static ProdutoController produtoController;
+	public static ArrayList<Venda> vendas;
+	public static VendaController vendaController;
+	
 	public static Scanner in = new Scanner (System.in);
 	
 	public static void mostrar() {
@@ -45,10 +55,18 @@ public class MenuView {
 		
 		menu.getActionList().add(menuProduto);
 		
-		
 		Menu menuVenda = new Menu();
 		menuVenda.setTitle("Menu Venda");
 		menuVenda.setName("Alterar e visualizar Vendas");
+		cadastrar = new Action("Cadastrar", new CadastrarVenda());
+		remover = new Action("Remover", new RemoverVenda());
+		modificar = new Action("Modificar", new ModificarVenda());
+		exibir = new Action("Exibir", new ExibirVenda());
+		menuVenda.getActionList().add(cadastrar);
+		menuVenda.getActionList().add(remover);
+		menuVenda.getActionList().add(modificar);
+		menuVenda.getActionList().add(exibir);
+		
 		menu.getActionList().add(menuVenda);
 		
 		menu.run();
